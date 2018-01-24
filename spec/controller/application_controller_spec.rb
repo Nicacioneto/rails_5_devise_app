2dc7a8e1a88e6cc06a0a9fd26bd6b56b9191a770
Nicacioneto <nicacionetobsb@hotmail.com>
2018-01-24 19:10:38 -0200

    test get application controller route

--- spec/controller/application_controller_spec.rb
+++ spec/controller/application_controller_spec.rb
@@ -9,12 +9,10 @@ RSpec.describe ApplicationController, type: :controller do
       expect(subject.current_user).to_not eq(nil)
     end
 
-    # it "should get index" do
-    #   # Note, rails 3.x scaffolding may add lines like get :index, {}, valid_session
-    #   # the valid_session overrides the devise login. Remove the valid_session from your specs
-    #   get 'index'
-    #   response.should be_success
-    # end
-
-
+    it "should get new session" do
+      # Note, rails 3.x scaffolding may add lines like get :index, {}, valid_session
+      # the valid_session overrides the devise login. Remove the valid_session from your specs
+      get :home
+      expect(response).to be_success
+    end
 end
